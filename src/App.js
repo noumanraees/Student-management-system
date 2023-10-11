fc01a14aa2c963522abd28346c93aaecb2292c32
Nouman <noumanraees>
2023-10-12 03:31:06 +0500

    Feat: Homepage is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/Homepage/Homepage';
+
 
 function App() {
     return ( 
         <div className = "App" >
-
+        <Homepage/>
+        
         </div>
     );
 }
